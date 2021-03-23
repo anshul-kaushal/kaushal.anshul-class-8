84fe3b7664aa1ccedf96194c96fc93b32d96f1ba
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-03-24 01:33:36 +0530

    Redisplay the menu and add some more changes

--- calc.sh
+++ calc.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
-
+function get_numbers {
+echo "Please enter the first number:"
+read first_number
+echo "Please enter the second number:"
+read second_number
+}
+program=1
 # When the program is first loaded, display a greeting to the user.
 echo "Welcome user! I'm calculator. Would you be kind enough to tell me your name?"
 read username
 if [[ -z $username ]]; then
 echo "Oh! It's fine if you don't want to provide your name."
+username="user"
 else
 echo "Welcome $username!"
 fi
@@ -12,6 +19,10 @@ fi
   # Add
   # Subtract
   # Exit
+until [[ $program -eq 0 ]]; do
+echo ""
+echo "------------CALCULATOR------------"
+echo ""
 echo "Press 1 for add"
 echo "Press 2 for subtract"
 echo "Press 3 for multiplication"
@@ -21,33 +32,37 @@ echo "Press 0 to exit the program"
 read user_response
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
-echo "Please enter the first number, $username:"
-read first_number
-echo "and the second number:"
-read second_number
-echo "Thank you $username"
+# When the operation is complete, redisplay the menu.
+if [[ $user_response -eq 0 ]]; then
+echo "Thank you for your time! Exiting program...."
+program=0
+else
 case $user_response in 
 	"1")
-	echo "Your answer for $first_number + $second_number is:"
+	get_numbers
+	echo "Your answer for $first_number + $second_number is:" 
 	expr $first_number + $second_number
 	;;
 	"2")
+	get_numbers
 	echo "Your answer for $first_number -  $second_number is:"
 	expr $first_number - $second_number
 	;;
 	"3")
+	get_numbers
 	echo "Your answer for $first_number * $second_number is:"
 	expr $first_number "*" $second_number
 	;;
 	"4")
+	get_numbers
 	echo "Your answer for $first_number / $second_number is:"
 	expr $first_number / $second_number
-	;;
-	"0")
-	echo "Thank you for your time! Exiting program"
-	;; 	
+	;;	
 	*)
-	echo "Please choose the correct option"
+	echo "Please choose the correct operation, $username"
 	;;
 esac
-# When the operation is complete, redisplay the menu.
+echo ""
+echo "Thank you $username"
+fi
+done
