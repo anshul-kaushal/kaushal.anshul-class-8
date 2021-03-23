9a3e191500cf833acf59ae097c4b4a9c34c3325a
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-03-24 01:37:24 +0530

    Make the code more readable

--- calc.sh
+++ calc.sh
@@ -1,20 +1,26 @@
 #! /usr/bin/env bash
+
 function get_numbers {
 echo "Please enter the first number:"
 read first_number
 echo "Please enter the second number:"
 read second_number
 }
+
 program=1
+
 # When the program is first loaded, display a greeting to the user.
+
 echo "Welcome user! I'm calculator. Would you be kind enough to tell me your name?"
 read username
+
 if [[ -z $username ]]; then
 echo "Oh! It's fine if you don't want to provide your name."
 username="user"
 else
 echo "Welcome $username!"
 fi
+
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
@@ -28,11 +34,14 @@ echo "Press 2 for subtract"
 echo "Press 3 for multiplication"
 echo "Press 4 for division"
 echo "Press 0 to exit the program"
+
 # Then, capture the user selection.
 read user_response
+
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+
 if [[ $user_response -eq 0 ]]; then
 echo "Thank you for your time! Exiting program...."
 program=0
@@ -62,7 +71,9 @@ case $user_response in
 	echo "Please choose the correct operation, $username"
 	;;
 esac
+
 echo ""
 echo "Thank you $username"
+
 fi
 done
